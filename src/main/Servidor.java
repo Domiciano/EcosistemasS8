6e9f4bb2e28f4add3c77a05fdda668f49b2a4427
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2020-10-01 12:48:51 -0500

    ellipse ahora si

--- src/main/Servidor.java
+++ src/main/Servidor.java
@@ -7,10 +7,11 @@ public class Servidor extends PApplet{
 	
 	public static void main(String[] args) {
 		PApplet.main("main.Servidor");
+		
 	}
 	
 	public void settings() {
-		
+		size(500,500); 
 	}
 	
 	public void setup() {
@@ -18,7 +19,8 @@ public class Servidor extends PApplet{
 	}
 	
 	public void draw() {
-		
+		background(255,255,0); 
+		ellipse(width/2, height/2, 50,50); 
 		
 	}
 }
