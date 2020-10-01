0ab6f712b63e514d7a84450c4dc1b0ee31e827c2
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2020-10-01 12:39:46 -0500

    ellipse en centro

--- src/main/Servidor.java
+++ src/main/Servidor.java
@@ -19,12 +19,13 @@ public class Servidor extends PApplet{
 	
 	
 	public static void main(String[] args) {
+		PApplet.main("main.Servidor");
 		
 
 	}
 	
 	public void settings() {
-		
+		size(500,500); 
 	}
 	
 	public void setup() {
@@ -35,7 +36,8 @@ public class Servidor extends PApplet{
 	
 	
 	public void draw() {
-		
+	background(255,255,0); 
+	ellipse(width/2, height/2, 100,100); 
 		
 	}
 		
