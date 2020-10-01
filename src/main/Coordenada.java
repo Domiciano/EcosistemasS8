29f62d8360963d3e9cca0d1c9185c82e2c57f47b
Jhon Garcia <metalprime57@gmail.com>
2020-10-01 12:57:29 -0500

    update Coordinate

--- src/main/Coordenada.java
+++ src/main/Coordenada.java
@@ -2,5 +2,35 @@ package main;
 
 public class Coordenada {
 	
+	private String x;
+	private String y;
+	
+	public Coordenada (String x, String y) {
+		this.setX(x);
+		this.setY(y);
+		
+	}
+	
+	public Coordenada () {
+		
+	}
+
+	public String getY() {
+		return y;
+	}
+
+	public void setY(String y) {
+		this.y = y;
+	}
+
+	public String getX() {
+		return x;
+	}
+
+	public void setX(String x) {
+		this.x = x;
+	}
+	
+	
 	
 }
