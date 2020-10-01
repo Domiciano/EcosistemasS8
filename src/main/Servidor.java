29a005c5058e9b9bff6d190be4001fea364a917f
Domiciano <domiciano.rincon@correounivalle.edu.co>
2020-10-01 12:04:30 -0500

    Liempieza hecha

--- src/main/Servidor.java
+++ src/main/Servidor.java
@@ -1,18 +1,5 @@
 package main;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-
-import com.google.gson.Gson;
-
 import processing.core.PApplet;
 
 public class Servidor extends PApplet{
@@ -20,7 +7,6 @@ public class Servidor extends PApplet{
 	
 	public static void main(String[] args) {
 		
-
 	}
 	
 	public void settings() {
@@ -29,16 +15,10 @@ public class Servidor extends PApplet{
 	
 	public void setup() {
 		
-		
 	}
-	
-	
 	
 	public void draw() {
 		
 		
 	}
-		
-	
-
 }
